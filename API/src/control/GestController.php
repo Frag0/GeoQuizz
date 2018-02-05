0f4a97eaa7c56e631f09cd9edfdd8fb9d4e39701
Maxime <maximesd52@gmail.com>
2018-02-05 13:20:29 +0100

    controllers

---
+++ API/src/control/GestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace geo\control;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \lbs\model\Photo;
+use \lbs\model\Score;
+use \lbs\model\Photo;
+use \lbs\model\Niveau;
+use \lbs\model\User;
+use \lbs\model\Ville;
+use \lbs\model\Serie;
+
+class GeoController {
+
+}
---
+++ API/src/control/PlayerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace geo\control;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \geo\model\Photo;
+use \geo\model\Score;
+use \geo\model\Photo;
+use \geo\model\Niveau;
+use \geo\model\User;
+use \geo\model\Ville;
+use \geo\model\Serie;
+
+class GeoController {
+
+}
