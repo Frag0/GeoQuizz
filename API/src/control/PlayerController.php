7b35cc832fa4f2790c8131eb3cb4f027418c3d04
Julien <charlesj52130@gmail.com>
2018-02-06 11:32:36 +0100

    json parties

--- API/src/control/PlayerController.php
+++ API/src/control/PlayerController.php
@@ -55,7 +55,7 @@ class PlayerController {
             "photos"=>$photos
         ];
         $resp = $resp->withStatus(201);
-        $resp = $resp->withJson($tabphoto);        
+        $resp = $resp->withJson($tabphotos);        
         return $resp;
     }
 
