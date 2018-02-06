a7851e566cda24071aae5866a5ccb28a35ba5f95
Gauthier <core.editor>
2018-02-06 15:03:52 +0100

    options

--- API/api/rest.php
+++ API/api/rest.php
@@ -22,6 +22,20 @@ $configuration = array_merge($settings, $errors, $dependencies);
 $c = new \Slim\Container($configuration);
 $app = new \Slim\App($c);
 
+$app->add(function($request, $response, callable $next){
+    $response = $next($request, $response);
+    $response = $response->withHeader('Content-type', 'application/json; charset=utf-8');
+    $response = $response->withHeader("Access-Control-Allow-Headers", "authorization, Content-Type");
+    $response = $response->withHeader('Access-Control-Allow-Origin', $request->getHeader('Origin')[0]);
+    $response = $response->withHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+    return $response;
+});
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
 $app->get('/series/{id}[/]','\geo\control\PlayerController:getSerieId');
 
 $app->get('/series[/]','\geo\control\PlayerController:getSeries');
