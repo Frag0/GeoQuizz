f1207415de3144b8dd72a5dd2ef0f8d88c2e304e
Maxime <maximesd52@gmail.com>
2018-02-06 14:15:17 +0100

    js

--- GESTION/src/components/MembreCreation.vue
+++ GESTION/src/components/MembreCreation.vue
@@ -1,6 +1,21 @@
 <template>
 	<div class="card" style="width: 18rem;">
-		Coucou
+		<form @submit="creerMembre">
+			<div class="form-group">
+				<label for="Identifiant">Identifiant</label>
+				<input type="text" v-model="identifiant" class="form-control" id="identifiant" placeholder="Identifiant">
+			</div>
+			<div class="form-group">
+				<label for="password">Mot de passe</label>
+				<input type="password" v-model="password" class="form-control" id="password" placeholder="Mot de passe">
+			</div>
+			<div class="form-group">
+				<label for="password">Vérifier le mot de passe</label>
+				<input type="password" v-model="verif" class="form-control" id="verif" placeholder="Vérifier le mot de passe">
+			</div>
+			<center><button type="submit" class="btn btn-outline-primary">Valider</button></center>
+			<center><router-link to="/connexion">Se connecter</router-link></center>
+		</form>
 	</div>
 </template>
 
@@ -10,10 +25,28 @@ export default {
 	name: 'MembreCreation',
 	data () {
 		return {
+		  identifiant: '',
+	      password: '',
+	      verif:''
 		}
 	},
 	methods: {
-
+		creerMembre() {
+	      if (this.verif!==this.password) {
+	        alert('Les mots de passe ne correspondent pas')
+	      }
+	      else {
+	        window.axios.post('users', {
+	          identifiant: this.identifiant,
+	          password: this.password,
+	        }).then(response => {
+	          console.log('Le membre '+response.data.identifiant+' a été créé')
+	          this.$router.push({path: '/connexion'})
+	        }).catch((error) => {
+	          alert(error.response.data.error.join("\n"))
+	        })
+	      }
+    	}
 	}
 }
 </script>
---
+++ GESTION/src/components/SerieCreation.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="card" style="width: 18rem;">
+		<form @submit="creerSerie">
+			<div class="form-group">
+				<label for="Ville">Ville</label>
+				<input type="text" v-model="ville" class="form-control" id="ville" placeholder="Ville">
+			</div>
+
+			<center><button type="submit" class="btn btn-outline-primary">Créer</button></center>
+
+		</form>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'SerieCreation',
+	data () {
+		return {
+			ville: '',
+		}
+	},
+	methods: {
+		creerSerie(){
+			window.axios.post('series', {
+	        ville: this.ville,
+	      }).then(response => {
+	        //this.$router.push({path: '/series'});  
+	      })
+		}
+	}
+}
+</script>
+
+<style scoped>
+.card {
+	top: 30vh;
+	padding: 10px;
+	margin: 0 auto;
+	float: none;
+	margin-bottom: 10px;
+}
+</style>
