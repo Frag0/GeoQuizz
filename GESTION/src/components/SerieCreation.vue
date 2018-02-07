2dbd2a80032088cb68d9fa0e198a0678fd179647
Gauthier <core.editor>
2018-02-07 14:56:04 +0100

    carte serie

--- GESTION/src/components/SerieCreation.vue
+++ GESTION/src/components/SerieCreation.vue
@@ -1,26 +1,14 @@
 <template>
-	<div class="card" style="width: 18rem;">
+	<div class="card" style="width: 40rem;">
 		<form @submit="creerSerie">
 			<div class="form-group">
 				<label for="Ville">Ville</label>
 				<input type="text" v-model="ville" class="form-control" id="ville" placeholder="Ville">
 			</div>
-			<div class="form-group">
-				<label for="Longitude">Longitude</label>
-				<input type="text" v-model="longitude" class="form-control" id="longitude" placeholder="Longitude">
-			</div>
-			<div class="form-group">
-				<label for="Latitude">Latitude</label>
-				<input type="text" v-model="latitude" class="form-control" id="latitude" placeholder="Latitude">
-			</div>
+			<label for="mapid">Veuillez positionnez le marqueur sur la carte</label>
+			<div id="mapid" style="width: 100%; height: 400px;"></div>
 			<center><button type="submit" class="btn btn-outline-primary">Créer</button></center>
 		</form>
-		<div>
-			<v-map :zoom=13 :center="[47.413220, -1.219482]">
-				<v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
-				<v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
-			</v-map>
-		</div>
 	</div>
 </template>
 
@@ -33,18 +21,47 @@ export default {
 		return {
 			ville: '',
 			longitude: null,
-			latitude: null,
+			latitude: null
 		}
+	},
+	mounted(){
+		var mymap = L.map('mapid').setView([48.8589507,2.2770204], 5);
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.streets'
+		}).addTo(mymap);
+		var marker = 0;
+		function onMapClick(e) {
+			if (marker == 0) {
+				marker = L.marker().setLatLng(e.latlng).addTo(mymap)
+				window.bus.longitude = marker.getLatLng().lng
+				window.bus.latitude = marker.getLatLng().lat
+			}else{
+				marker.setLatLng(e.latlng)
+				window.bus.longitude = marker.getLatLng().lng
+				window.bus.latitude = marker.getLatLng().lat
+			}
+		};
+		mymap.on('click', onMapClick)
 	},
 	methods: {
 		creerSerie(){
-			window.axios.post('series', {
-		        ville: this.ville,
-		        longitude: this.longitude,
-		        latitude: this.latitude
-	      }, {headers:  {'Authorization': 'Bearer ' + this.$store.state.member.token }}).then(response => {
-	        this.$router.push({path: '/bienvenue'});  
-	      })
+			if (window.bus.longitude != null && window.bus.latitude != null && this.ville != '') {
+				window.axios.post('series', {
+		        	ville: this.ville,
+		        	longitude: window.bus.longitude,
+		        	latitude: window.bus.latitude
+	    		}, {headers:  {'Authorization': 'Bearer ' + this.$store.state.member.token }})
+	    		.then(response => {
+	        		this.$router.push({path: '/bienvenue'});  
+	      		})
+			}else{
+				alert("Veuillez placer le marqueur et remplir le champ ville ! ")
+			}
+			
 		}
 	}
 }
@@ -53,7 +70,7 @@ export default {
 <style scoped>
 @import "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css";
 .card {
-	top: 30vh;
+	top: 15vh;
 	padding: 10px;
 	margin: 0 auto;
 	float: none;
