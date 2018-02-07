5d9e0cb2466eb59dac3eaea0777a44a176bd503f
bvarnerot <pamlas.bv@gmail.com>
2018-02-07 16:21:07 +0100

    oui c'est presque fini

--- CLIENT/src/components/AfficherJeu.vue
+++ CLIENT/src/components/AfficherJeu.vue
@@ -13,60 +13,42 @@ export default {
 		return {
 			long : 2.3488000,
 			lat : 48.8534100,
-			series : [],
 			photos : [],
 			ok : true,
 			url : 'https://www.novap.fr/media/catalog/product/cache/1/image/650x650/9df78eab33525d08d6e5fb8d27136e95/p/n/4160306-330x200-en-reparation-pas-mettre-marche.jpg'
 		}
 	},
-	created(){
-		/*window.axios.get('series').then(response => {
-			this.series = response.data.series			
-		})*/
-		//console.log(this.$store.getters.getId); 
-		
-		
-	},
-	mounted(){
-
-	},
-
-	
 
 	methods : {
-		window.axios.get('series/'+this.$store.getters.getId+'/photos').then(response => {
-				this.photos = response.data.photos
-				console.log(response.data.photos);
-		}),
 		setMap(){
-			
-			console.log(this.photos[0]);
+			window.axios.get('series/'+this.$store.getters.getId+'/photos').then(response => {
+				this.photos = response.data.photos;
+				this.url= this.photos[0].url;
+				this.ok = false;
 
-			this.url= this.photos[0].url;
-			this.ok = false;
+				var mymap = L.map('mapid').setView([this.$store.getters.getLongitude,this.$store.getters.getLatitude], 11);
+				var $message = "Bienvenue sur le premier jeu au monde intéractif avec des monuments";
+				L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+					maxZoom: 25,
+					id: 'mapbox.streets'
+				}).addTo(mymap);
+				L.marker([this.$store.getters.getLongitude,this.$store.getters.getLatitude], {draggable : true}).addTo(mymap).bindPopup($message).openPopup();
 
-			var mymap = L.map('mapid').setView([this.$store.getters.getLongitude,this.$store.getters.getLatitude], 11);
-			var $message = "Bienvenue sur le premier jeu au monde intéractif avec des monuments";
-			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-				maxZoom: 25,
-				id: 'mapbox.streets'
-			}).addTo(mymap);
-			L.marker([this.series[0].longitude,this.series[0].latitude], {draggable : true}).addTo(mymap).bindPopup($message).openPopup();
+				var lat = this.lat;
+				var lng = this.long;
 
-			var lat = this.lat;
-			var lng = this.long;
+				function onMapClick(e) {
+					L.marker().setLatLng(e.latlng).addTo(mymap).bindPopup("Tu as cliqué la").openPopup();
 
-			function onMapClick(e) {
-				L.marker().setLatLng(e.latlng).addTo(mymap).bindPopup("Tu as cliqué la").openPopup();
+					var distance = L.latLng([lat,lng]).distanceTo(e.latlng);
 
-				var distance = L.latLng([lat,lng]).distanceTo(e.latlng);
-
-				if(distance < 3000){
-					alert("Bien joué poto");
+					if(distance < 3000){
+						alert("Bien joué poto");
+					}
 				}
-			}
 
-			mymap.on('click', onMapClick);
+				mymap.on('click', onMapClick);
+			});		
 		}
 	}
 }
