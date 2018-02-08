0f5e47f41639b60ff130d02de16f8ce78363735b
Maxime <maximesd52@gmail.com>
2018-02-08 17:10:03 +0100

    scores

--- CLIENT/src/router/index.js
+++ CLIENT/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Accueil from '@/components/Accueil'
 import AfficherJeu from '@/components/AfficherJeu'
+import AfficherScores from '@/components/AfficherScores'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/jeu',
       name: 'AfficherJeu',
       component: AfficherJeu
+    },
+    {
+      path: '/scores',
+      name: 'AfficherScores',
+      component: AfficherScores
     }
   ]
 })
